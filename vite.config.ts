a6809af543f83ad69499b1e82e8919785d8b3f66
josevc1 <38393009+josevc1@users.noreply.github.com>
2024-07-13 12:34:59 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: [inject({ Buffer: ["Buffer", "Buffer"] })],
+      plugins: [inject({ Buffer: ["Buffer", "Buffer"] })],
     },
   },
 });
